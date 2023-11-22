21275a702bd798f08f35a94ccd0fe2aea82a210e
Evgeniy Zudin <evgeniy.zudin@mera.com>
2023-11-22 08:55:39 +0100

    add listener

--- app/src/main/java/com/example/imagegallery/ItemAdapter/ItemAdapter.kt
+++ app/src/main/java/com/example/imagegallery/ItemAdapter/ItemAdapter.kt
@@ -5,21 +5,38 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import androidx.appcompat.view.menu.MenuView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.imagegallery.R
 import com.example.imagegallery.model.Images
 
 class ItemAdapter(private val context:Context,
                   val dataSet:List<Images>):RecyclerView.Adapter<ItemAdapter.ImageViewHolder>(){
-    class ImageViewHolder(val view: View):RecyclerView.ViewHolder(view){
+
+    lateinit var mListener: onItemClickListener
+    interface onItemClickListener{
+        fun onItemClick(position: Int)
+    }
+
+    fun setOnItemClickListener(listener: onItemClickListener) {
+
+        mListener = listener
+
+    }
+
+    class ImageViewHolder(val view: View, listener: onItemClickListener):RecyclerView.ViewHolder(view){
         val imageView:ImageView = view.findViewById(R.id.item_image)
+
+        init {
+            itemView.setOnClickListener {
+                listener.onItemClick(adapterPosition)
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
         val adapterLayout = LayoutInflater.from(parent.context)
             .inflate(R.layout.item_list, parent, false)
-        return ImageViewHolder(adapterLayout)
+        return ImageViewHolder(adapterLayout, mListener)
     }
 
     override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
--- app/src/main/java/com/example/imagegallery/MainActivity.kt
+++ app/src/main/java/com/example/imagegallery/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.imagegallery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.imagegallery.ItemAdapter.ItemAdapter
 import com.example.imagegallery.dataSource.dataSource
@@ -14,11 +15,39 @@ class MainActivity : AppCompatActivity() {
         val recyclerView= findViewById<RecyclerView>(R.id.recyclerView)
         val recyclerView2= findViewById<RecyclerView>(R.id.recyclerView2)
         val recyclerView3= findViewById<RecyclerView>(R.id.recyclerView3)
-        recyclerView.adapter =ItemAdapter(this, dataPassed)
-        recyclerView2.adapter =ItemAdapter(this, dataPassed)
-        recyclerView3.adapter =ItemAdapter(this, dataPassed)
+
+
+
+        val adapter = ItemAdapter(this, dataPassed)
+        recyclerView.adapter = adapter
+        adapter.setOnItemClickListener(object: ItemAdapter.onItemClickListener{
+            override fun onItemClick(position: Int) {
+                Toast.makeText(this@MainActivity, "Clicked $position", Toast.LENGTH_SHORT).show()
+
+            }
+
+    })
+
+        val adapter2 = ItemAdapter(this, dataPassed)
+        recyclerView2.adapter = adapter2
+        adapter2.setOnItemClickListener(object: ItemAdapter.onItemClickListener{
+            override fun onItemClick(position: Int) {
+                Toast.makeText(this@MainActivity, "Clicked $position", Toast.LENGTH_SHORT).show()
+
+            }
+        })
+
+        val adapter3 = ItemAdapter(this, dataPassed)
+        recyclerView3.adapter = adapter3
+        adapter3.setOnItemClickListener(object: ItemAdapter.onItemClickListener{
+            override fun onItemClick(position: Int) {
+                Toast.makeText(this@MainActivity, "Clicked $position", Toast.LENGTH_SHORT).show()
+
+            }
+
+        })
         recyclerView.setHasFixedSize(true)
         recyclerView2.setHasFixedSize(true)
         recyclerView3.setHasFixedSize(true)
-    }
+}
 }
